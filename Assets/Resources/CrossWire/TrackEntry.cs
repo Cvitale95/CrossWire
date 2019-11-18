9fdaa4247b980425afc7abc578c60d08d1e15881
Evan Gonzales <gonz6739@csumb.ad.csumb.edu>
2019-11-18 11:47:51 -0800

    Entry tracking testing

---
+++ Assets/Resources/CrossWire/TrackEntry.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackEntry : MonoBehaviour
+{
+    private List<Collider> lastObjects = new List<Collider>();
+    private int maxSize = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(lastObjects.Count == maxSize)
+        {
+            Collider last = lastObjects[0];
+            if(last != null)
+                Colorize(last, true);
+            lastObjects.RemoveAt(0);
+        }
+
+        lastObjects.Add(other);
+        Colorize(other, false);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        lastObjects.Remove(other);
+        SetMatColor(other, Color.gray);
+    }
+
+    public void Colorize(Collider collider, bool red)
+    {
+        SetMatColor(collider, red ? Color.red : Color.green);
+    }
+
+    public void SetMatColor(Collider collider, Color color)
+    {
+        Renderer renderer = collider.GetComponent<Renderer>();
+        renderer.material.SetColor("_Color", color);
+    }
+}
--- Assets/Scenes/SpawnCube.cs
+++ Assets/Scenes/SpawnCube.cs
@@ -5,6 +5,7 @@ using Random = System.Random;
 
 public class SpawnCube : MonoBehaviour
 {
+    private static bool spam;
     private static Material glowMaterial;
     private static Texture glowSprite;
     private Random rng = new Random();
@@ -27,17 +28,26 @@ public class SpawnCube : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.position = new Vector3(0, 5, 0);
-            cube.AddComponent<Rigidbody>();
+            spam = !spam;
+        }
 
-            Renderer renderer = cube.GetComponent<Renderer>();
+        if (spam) {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(rng.Next(-5, 5), 15, rng.Next(-5, 5));
+                cube.AddComponent<Rigidbody>();
 
-            Material tmp = renderer.sharedMaterial = new Material(Shader.Find("MK/Glow/Selective/Sprites/Default"));
-            tmp.SetColor("_Color", GetRandomColor());
-            tmp.SetColor("_MKGlowColor", GetRandomColor());
-            tmp.SetFloat("_MKGlowPower", GetRandomNumber(0, 2.5f));
-            tmp.SetTexture("_MKGlowTex", glowSprite);
+                Renderer renderer = cube.GetComponent<Renderer>();
+
+                Material tmp = renderer.sharedMaterial = new Material(Shader.Find("MK/Glow/Selective/Sprites/Default"));
+                tmp.SetColor("_Color", GetRandomColor());
+                tmp.SetColor("_MKGlowColor", GetRandomColor());
+                tmp.SetFloat("_MKGlowPower", GetRandomNumber(0, 0.2f));
+                tmp.SetTexture("_MKGlowTex", glowSprite);
+
+                Destroy(cube, 30);
+            }
         }
     }
 
