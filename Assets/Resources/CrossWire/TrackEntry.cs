69576596198531449a91588c8be7aa047c06ebde
Evan Gonzales <gonz6739@csumb.ad.csumb.edu>
2019-12-04 12:31:12 -0800

    Test

--- Assets/Resources/CrossWire/TrackEntry.cs
+++ Assets/Resources/CrossWire/TrackEntry.cs
@@ -1,52 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TrackEntry : MonoBehaviour
 {
-    private List<Collider> lastObjects = new List<Collider>();
+    private static int tracking = 0;
+    private List<string> correct = new List<string> { "1", "2" };
+    private List<string> lastObjects = new List<string>();
     private int maxSize = 30;
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start(){}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (lastObjects.SequenceEqual(correct))
+        {
+            gameObject.GetComponent<Renderer>().sharedMaterial.SetColor("_Color", new Color(0f, 1f, 0f));
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().sharedMaterial.SetColor("_Color", new Color(1f, 0f, 0f));
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        if(lastObjects.Count == maxSize)
-        {
-            Collider last = lastObjects[0];
-            if(last != null)
-                Colorize(last, true);
-            lastObjects.RemoveAt(0);
-        }
-
-        lastObjects.Add(other);
-        Colorize(other, false);
+        lastObjects.Add(other.name);
     }
 
     public void OnTriggerExit(Collider other)
     {
-        lastObjects.Remove(other);
-        SetMatColor(other, Color.gray);
-    }
-
-    public void Colorize(Collider collider, bool red)
-    {
-        SetMatColor(collider, red ? Color.red : Color.green);
-    }
-
-    public void SetMatColor(Collider collider, Color color)
-    {
-        Renderer renderer = collider.GetComponent<Renderer>();
-        renderer.material.SetColor("_Color", color);
+        lastObjects.Remove(other.name);
     }
 }
